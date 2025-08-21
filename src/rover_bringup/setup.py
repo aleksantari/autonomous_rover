f09b394e0179734a862237291781ec912c082682
Aleksantari <aleksantari@gmail.com>
2025-08-21 16:29:44 -0400

    setup and package.xml set

--- src/rover_bringup/setup.py
+++ src/rover_bringup/setup.py
@@ -3,21 +3,26 @@ import os
 from glob import glob
 
 package_name = 'rover_bringup'
+launch_files = glob(os.path.join('rover_bringup','launch', '*.launch.py'))
+config_files = glob(os.path.join('rover_bringup','config', '*.yaml'))
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', launch_files),
+        ('share/' + package_name + '/config', config_files),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='santari',
     maintainer_email='aleksantari@gmail.com',
-    description='TODO: Package description',
+    description='bringup package lauching Waveshare nodes + EKF',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
